1e49e1b096e8c7376aa1c6cbff5ea1d90c1b8220
Giorgi <129410101+giorgisar@users.noreply.github.com>
2023-07-31 00:52:38 +0400

    Update index.tsx

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import Logo from "@/assets/Logo.png";
-import { LinkIcon } from "@heroicons/react/20/solid";
 import Link from "./Link";
 import { SelectedPage } from "@/shared/types";
 import useMediaQuery from "@/hooks/useMediaQuery";
